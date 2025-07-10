23a862fa75fd49549983716ddc7c7f2593d739dc
James Morrissey <jammim19@gmail.com>
2025-07-10 13:28:01 +0100

    Added a component for the feedback result

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,15 @@ import { useState } from "react";
 import "./main.css";
 import FeedbackForm from "./FeedbackForm";
 import "./axeTestingHandler";
+import FeedbackResult from "./FeedbackResult";
 
 function App() {
   return (
     <>
       <main>
         <FeedbackForm></FeedbackForm>
-        <div className="attribution">
-          {/*  Rating state end  */}
-          {/*  Thank you state start  */}
-          You selected {/* Add rating here */} out of 5 Thank you! We appreciate
-          you taking the time to give a rating. If you ever need more support,
-          don’t hesitate to get in touch!
-          {/*  Thank you state end  */}
-        </div>
+        <FeedbackResult></FeedbackResult>
+        <div className="attribution"></div>
       </main>
     </>
   );
---
+++ src/FeedbackResult.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function FeedbackResult() {
+  return (
+    <article id="FeedbackResult">
+      You selected {/* Add rating here */} out of 5 Thank you! We appreciate you
+      taking the time to give a rating. If you ever need more support, don’t
+      hesitate to get in touch!
+    </article>
+  );
+}
+
+export default FeedbackResult;
