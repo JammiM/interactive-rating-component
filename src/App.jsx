db9565062e16668039f8d54ccb0fef02adf363dd
James Morrissey <jammim19@gmail.com>
2025-07-10 13:22:39 +0100

    Added Axe-core for testing accessibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
 import "./main.css";
 import FeedbackForm from "./FeedbackForm";
+import "./axeTestingHandler";
 
 function App() {
   return (
     <>
-      <FeedbackForm></FeedbackForm>
-      <div className="attribution">
-        {/*  Rating state end  */}
-        {/*  Thank you state start  */}
-        You selected {/* Add rating here */} out of 5 Thank you! We appreciate
-        you taking the time to give a rating. If you ever need more support,
-        don’t hesitate to get in touch!
-        {/*  Thank you state end  */}
-      </div>
+      <main>
+        <FeedbackForm></FeedbackForm>
+        <div className="attribution">
+          {/*  Rating state end  */}
+          {/*  Thank you state start  */}
+          You selected {/* Add rating here */} out of 5 Thank you! We appreciate
+          you taking the time to give a rating. If you ever need more support,
+          don’t hesitate to get in touch!
+          {/*  Thank you state end  */}
+        </div>
+      </main>
     </>
   );
 }
