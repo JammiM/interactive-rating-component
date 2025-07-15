510ae1c43486c5a9db952e65ffbf9fe6244737eb
James Morrissey <jammim19@gmail.com>
2025-07-15 13:25:45 +0100

    Updated the Feedback result component

--- src/FeedbackResult.jsx
+++ src/FeedbackResult.jsx
@@ -1,14 +1,25 @@
 import React, { useContext } from "react";
 import { SharedContext } from "./context/FunctionProvider";
+import thankYouLogo from "./assets/starter-files/images/illustration-thank-you.svg";
 
 function FeedbackResult() {
   const { sharedValue } = useContext(SharedContext);
 
   return (
-    <article id="FeedbackResult">
-      You selected <strong>{sharedValue}</strong> out of 5 Thank you! We
-      appreciate you taking the time to give a rating. If you ever need more
-      support, don&rsquo;t hesitate to get in touch!
+    <article id="feedbackResultContainer">
+      <img
+        src={thankYouLogo}
+        className="feedbackResultContainer__logo"
+        alt="result logo"
+      />
+      <div className="feedbackResultContainer__value">
+        You selected {sharedValue} out of 5
+      </div>
+      <h1 className="feedbackResultContainer__heading">Thank you! </h1>
+      <p className="feedbackResultContainer__description">
+        We appreciate you taking the time to give a rating. If you ever need
+        more support, don&rsquo;t hesitate to get in touch!
+      </p>
     </article>
   );
 }
